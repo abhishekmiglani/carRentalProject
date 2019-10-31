5d24b77f4b8b52e88f0fe6a95471bcce8d5eac5e
ankush360 <32308280+ankush360@users.noreply.github.com>
2019-10-31 09:23:25 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarDetailsComponent } from './car-details/car-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:"/carDetails",Component:CarDetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
