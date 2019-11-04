1d532ce4760020f9d27fff2365a5bc20612ffba3
GarryIBM <55372768+GarryIBM@users.noreply.github.com>
2019-11-04 16:35:25 +0530

    checking out

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CarDetailsComponent } from './car-details/car-details.component';
+
 
 
 const routes: Routes = [];
