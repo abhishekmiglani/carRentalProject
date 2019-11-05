9a41fbcd20736f1c3e6a036e2c0c4caedb8918b5
bhushan96 <47738654+bhushan96@users.noreply.github.com>
2019-11-05 17:03:38 +0530

    updated car details

--- src/app/cardetail/cardetail.component.ts
+++ src/app/cardetail/cardetail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { UploadFileComponent } from 'app/upload-file/upload-file.component';
-import { Component, OnInit } from '@angular/core';
-declare var $: any;
+
+
 
 @Component({
   selector: 'app-cardetail',
@@ -23,13 +23,7 @@ export class CardetailComponent implements OnInit {
 
 
   ngOnInit() {
-    $(function() {
-      $("#sortMenu a").click(function() {
-        console.log("Hey!");
-        $("#selected").text($(this).text());
-        $("#selected").val($(this).text());
-      });
-    });
+    
   }
 
   
