1a50a0850f968cc7fd3a0cf053f5f8c16af02366
ManishPandeyOO7 <56245563+ManishPandeyOO7@users.noreply.github.com>
2019-11-05 10:32:16 +0530

    banner update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarSelectComponent } from './car-select/car-select.component';
+import { BanersComponent } from './baners/baners.component';
 
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: "", component:BanersComponent},{path: "car", component: CarSelectComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
