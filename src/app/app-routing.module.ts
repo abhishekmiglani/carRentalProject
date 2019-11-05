11865f9f7d990861b763df5bd6edd3b9e97d189f
bhushan96 <47738654+bhushan96@users.noreply.github.com>
2019-11-05 16:20:54 +0530

    routing2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CarSelectComponent } from './car-select/car-select.component';
 import { BanersComponent } from './baners/baners.component';
 import { CardetailComponent } from './cardetail/cardetail.component';
+import { PaymentOptionsComponent } from './payment-options/payment-options.component';
 
 
 
 const routes: Routes = [{path: "", component:BanersComponent},
 {path: "car", children:[{path:"", component: CarSelectComponent},
-{path: ":modelNo" , component: CardetailComponent}]}];
+{path: ":modelNo" , component: CardetailComponent},
+{path:"payment", component:PaymentOptionsComponent}]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cardetail/cardetail.component.ts
+++ src/app/cardetail/cardetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UploadFileComponent } from 'app/upload-file/upload-file.component';
 
 @Component({
   selector: 'app-cardetail',
@@ -8,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class CardetailComponent implements OnInit {
 
   constructor() { }
+  @ViewChild(UploadFileComponent,{static:false}) upload:UploadFileComponent;
+  modalState:boolean=false;
+
+
+  changeState(){
+    console.log("parent");
+  this.upload.open();
+  }
+
+
 
   ngOnInit() {
   }
