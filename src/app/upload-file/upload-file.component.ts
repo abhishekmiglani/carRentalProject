d98abb3d16a3d6fdb84ecb9347360763cf2acd9a
bhushan96 <47738654+bhushan96@users.noreply.github.com>
2019-11-14 14:19:53 +0530

    image preview

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -14,8 +15,11 @@ export class UploadFileComponent implements OnInit {
   url:any[]=[];
   public message: string;
   buttonState:boolean=false;
-  constructor() { }
-  preview(files) {
+
+  fileFront:File;
+  fileBack:File;
+  constructor(private http:HttpClient) { }
+  previewFront(files) {
     if (files.length === 0)
       return;
  
@@ -32,9 +36,25 @@ export class UploadFileComponent implements OnInit {
       this.url.push(reader.result); 
     }
   }
+  previewBack(files) {
+    if (files.length === 0)
+      return;
  
-  uploadFile(event,files) {
-    
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.url.push(reader.result); 
+    }
+  }
+  uploadFileFront(event,files) {
+    this.fileFront =files[0];
     for (let index = 0; index < event.length; index++) {
       const element = event[index];
       this.files.push(element.name)
@@ -42,9 +62,20 @@ export class UploadFileComponent implements OnInit {
         this.buttonState=true;
       }
     }
-    this.preview(files);  
+    this.previewFront(files);  
   }
 
+  uploadFileBack(event,files) {
+    this.fileBack = files[0];
+    for (let index = 0; index < event.length; index++) {
+      const element = event[index];
+      this.files.push(element.name)
+      if(this.files.length==2){
+        this.buttonState=true;
+      }
+    }
+    this.previewBack(files);  
+  }
   
   deleteAttachment(index) {
     this.url.splice(index,1);
@@ -60,7 +91,13 @@ export class UploadFileComponent implements OnInit {
     console.log("child");
     document.getElementById("openModal2").click();
   }
-  
+  onUploadFile() {
+    console.log("uploading....");
+    //Upload file here send a binary data
+    
+    this.http.post('src/assets/', this.fileFront)
+    .subscribe();
+    }
  
 
   ngOnInit() {
