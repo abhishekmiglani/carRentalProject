89c02dd6caf1710ed5195856783d90b6cff0d117
bhushan96 <47738654+bhushan96@users.noreply.github.com>
2019-11-14 12:32:20 +0530

    update upload-file

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -9,10 +9,31 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
   files: any = [];
+  public imagePath;
+  public imagePath2;
+  url:any[]=[];
+  public message: string;
   buttonState:boolean=false;
   constructor() { }
-
-  uploadFile(event) {
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.url.push(reader.result); 
+    }
+  }
+ 
+  uploadFile(event,files) {
     
     for (let index = 0; index < event.length; index++) {
       const element = event[index];
@@ -20,9 +41,13 @@ export class UploadFileComponent implements OnInit {
       if(this.files.length==2){
         this.buttonState=true;
       }
-    }  
+    }
+    this.preview(files);  
   }
+
+  
   deleteAttachment(index) {
+    this.url.splice(index,1);
     this.files.splice(index, 1);
     if(this.files.length==2){
       this.buttonState=true;
