d2004a8413ca952be004f16d142ebcebde754b25
ankush360 <32308280+ankush360@users.noreply.github.com>
2019-11-21 09:33:21 +0530

    Revert "ol"
    This reverts commit 9200f1aa6e7f5a5da52c2ea024b24b09e42ce0a5.

---
+++ src/app/Bean/Car.ts
@@ -0,0 +1,15 @@
+export interface Car {
+    id: number,
+    carName: string,
+    location: string,
+    city:string,
+    bookingPrice : number,
+    carType:string,
+    fuelType:string,
+    numOfSeats:number,
+    pricePerKm:number,
+    transmissionType:string,
+    imageUrl: string,
+    isBooked:boolean
+    
+}
