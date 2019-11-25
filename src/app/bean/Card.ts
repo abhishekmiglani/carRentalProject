914bddc126b0d588ccc3dad75e406e2c1252f7f2
Abhishek Miglani <42495528+abhishekmiglani@users.noreply.github.com>
2019-11-25 09:38:48 +0530

    changes

--- src/app/bean/Card.ts
+++ src/app/bean/Card.ts
@@ -1,3 +1,5 @@
+import { User } from './User';
+
 export class Card{
     cardId;
     cardNum;
@@ -5,4 +7,15 @@ export class Card{
     expMonth;
     expYear;
     cardHolderName;
+    userDetails:User;
+
+    constructor(cardId,cardNum,cvv,expMonth,expYear,cardHolderName,userDetails){
+        this.cardId = cardId;
+        this.cardNum = cardNum;
+        this.cvv = cvv;
+        this.expMonth = expMonth;
+        this.expYear = expYear;
+        this.cardHolderName = cardHolderName;
+        this.userDetails = userDetails;
+    }
 }
