e0a99ceb5fe9d8a0faaf78bf5d8f5ba5d8b14752
bhushan96 <47738654+bhushan96@users.noreply.github.com>
2019-11-26 08:43:43 +0530

    user bean

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -28,11 +28,13 @@ export class UserService {
     logout(){
       localStorage.clear();
     }
-    postLicenseFile(user:User,frontImg:File,backImg:File): Observable<Boolean> {
-      const endpoint = 'http://localhost:8081/users';
-      this.http.post("https://www.dropbox.com/request/HpWEP1Ix1tBDBwonS1sM",frontImg);
-      this.http.post("https://www.dropbox.com/request/HpWEP1Ix1tBDBwonS1sM",backImg);
-       return this.http.put<Boolean>(endpoint, user);
+    postLicenseFile(frontData,backData): Observable<Boolean> {
+      const endpoint = 'http://localhost:8099/userservice/users/abc@gmail.com';
+      
+       this.http.put<Boolean>(endpoint, frontData);
+      return this.http.put<Boolean>(endpoint, backData);
+       
+
         
 }
 }
